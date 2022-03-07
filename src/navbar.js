e49c4eaf0e430265bb8f6a123999b51f89bedec2
Lauro Jacques <lauro.jacques@dtidigital.com.br>
2022-03-07 14:35:08 -0300

    feat: :tada: initial commit

---
+++ src/navbar.js
@@ -0,0 +1,84 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import * as singleSpa from "single-spa";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+
+    switch (newValue) {
+      case 0:
+        singleSpa.navigateToUrl("/");
+        break;
+
+      case 1:
+        singleSpa.navigateToUrl("/app-react");
+        break;
+
+      case 2:
+        singleSpa.navigateToUrl("/app-angular");
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}></TabPanel>
+      <TabPanel value={value} index={1}></TabPanel>
+      <TabPanel value={value} index={2}></TabPanel>
+    </Box>
+  );
+}
---
+++ src/root.component.tsx
@@ -0,0 +1,5 @@
+import BasicTabs from "./navbar.js";
+
+export default function Root(props) {
+  return BasicTabs();
+}
